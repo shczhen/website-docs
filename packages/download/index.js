09d2d9127f4ce2a176bf26c8857e68ec988cb386
czhen <56986964+shczhen@users.noreply.github.com>
2022-08-10 11:56:49 +0800

    Update index.js

--- packages/download/index.js
+++ packages/download/index.js
@@ -58,25 +58,6 @@ export function download(argv) {
 
   switch (repo) {
     case 'pingcap/docs-cn':
-      if (ref === 'master') {
-        const newRef = 'master-0727'
-        retrieveAllMDsFromZip(
-          {
-            repo,
-            path,
-            ref: newRef,
-          },
-          genDest(
-            repo,
-            path,
-            nPath.resolve(
-              dest,
-              `${repo.endsWith('-cn') ? 'zh' : 'en'}/tidb/master`
-            )
-          ),
-          options
-        )
-      } else {
         retrieveAllMDsFromZip(
           {
             repo,
@@ -93,8 +74,6 @@ export function download(argv) {
           ),
           options
         )
-      }
-
       break
     case 'pingcap/docs':
       if (ref.startsWith('i18n-')) {
@@ -109,24 +88,6 @@ export function download(argv) {
             ref,
           },
           genDest(repo, path, nPath.resolve(dest, `${refLang}/tidb/${refVer}`)),
-          options
-        )
-      } else if (ref === 'master') {
-        const newRef = 'master-0727'
-        retrieveAllMDsFromZip(
-          {
-            repo,
-            path,
-            ref: newRef,
-          },
-          genDest(
-            repo,
-            path,
-            nPath.resolve(
-              dest,
-              `${repo.endsWith('-cn') ? 'zh' : 'en'}/tidb/master`
-            )
-          ),
           options
         )
       } else {
